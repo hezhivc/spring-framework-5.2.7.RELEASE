8f29198ee2cd26ccb53ab1a716d5599501b6a1c8
hezhi <hezhivc@163.com>
2021-11-11 13:58:00 +0800

    测试提交

--- my-spring-ioc/src/main/java/cn/zhe/hello/Test.java
+++ my-spring-ioc/src/main/java/cn/zhe/hello/Test.java
@@ -1,14 +1,7 @@
 package cn.zhe.hello;
 
-import java.io.File;
-
 public class Test {
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\zhe\\Desktop\\yht\\助贷后管上线\\数据库脚本.zip");
-		if(file.exists()){
-			file.delete();
-		} else {
-			System.out.println("file not exit");
-		}
+
 	}
 }
